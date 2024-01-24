281e2c7e888b3d395cda9c704b5a7680061aba30
Bryan Lee <bryanmylee@gmail.com>
2024-01-24 17:06:52 +0800

    refactor: remove game_client / game_server abstraction

--- project/multiplayer/game_client.gd
+++ project/multiplayer/game_client.gd
@@ -5,7 +5,7 @@ const _DEFAULT_TIMEOUT := 5.0
 var env_timeout := OS.get_environment("CLIENT_TIMEOUT")
 var timeout := float(env_timeout) if env_timeout else _DEFAULT_TIMEOUT
 
-const _DEFAULT_SERVER_HOST := "127.0.0.1"
+const _DEFAULT_SERVER_HOST := "multiplayer-test.bryanmylee.com" # "127.0.0.1"
 var env_server_host := OS.get_environment("SERVER_HOST")
 var server_host := env_server_host if env_server_host else _DEFAULT_SERVER_HOST
 
@@ -15,11 +15,41 @@ var server_port := int(env_server_port) if env_server_port else _DEFAULT_SERVER_
 
 ## We use this in place of `multiplayer.get_unique_id()` for more customization and static access via `Program.client`.
 var peer_id := 0
+var peer := WebSocketMultiplayerPeer.new()
 
 
 func _enter_tree() -> void:
 	Program.client = self
 
 
+func _ready() -> void:
+	start()
+
+	multiplayer.multiplayer_peer = peer
+	multiplayer.connected_to_server.connect(_handle_connected_to_server)
+	multiplayer.connection_failed.connect(_handle_server_connection_failed)
+	multiplayer.server_disconnected.connect(_handle_server_disconnected)
+
+
 func start() -> Result:
-	return Result.Err("missing implementation for `start`")
+	var protocol := "wss://" if Program.ssl_enabled else "ws://"
+	var address := protocol + server_host + ":" + str(server_port)
+	Logger.client_log(["connecting to game server at: ", address], ["init"])
+	return Result.from_gderr(peer.create_client(address))
+
+
+#region Network
+func _handle_connected_to_server() -> void:
+	peer_id = multiplayer.get_unique_id()
+	Logger.client_log(["connected to server"], ["init"])
+	GameNetwork.game_network_ready.emit()
+
+
+func _handle_server_connection_failed() -> void:
+	Logger.client_log(["failed to connect to server"], ["init"])
+
+
+func _handle_server_disconnected() -> void:
+	peer_id = 0
+	Logger.client_log(["server disconnected"], ["network"])
+#endregion
--- project/multiplayer/game_server.gd
+++ project/multiplayer/game_server.gd
@@ -12,11 +12,7 @@ const _DEFAULT_TIMEOUT := 5.0
 var env_timeout := OS.get_environment("SERVER_TIMEOUT")
 var timeout := float(env_timeout) if env_timeout else _DEFAULT_TIMEOUT
 
-"Dict<{
-	id: int;
-	rtc_ready: bool;
-}>"
-var clients := {}
+var peer := WebSocketMultiplayerPeer.new()
 
 
 func _enter_tree() -> void:
@@ -28,5 +24,66 @@ func _enter_tree() -> void:
 		queue_free()
 
 
+func _ready() -> void:
+	var start_result := start()
+	if start_result.is_err():
+		Logger.server_log(["failed to start server due to: ", start_result.unwrap_err()], ["init"])
+		# For a dedicated server, if we fail to start the server, we want to
+		# exit the program with an error code so that the matchmaking server
+		# can detect the failure.
+		if Program.is_dedicated_server:
+			OS.kill(OS.get_process_id())
+		# For prototyping, `_ready` is called by all clients but only one client
+		# will be able to bind to the listening port. Therefore, we can free all
+		# programs that failed to start the server and assume they are just clients.
+		elif OS.is_debug_build():
+			queue_free()
+		return
+	
+	Logger.server_log(["started server on port: ", port], ["init"])
+	# Get rid of this program's client.
+	Program.client.queue_free()
+	await Program.client.tree_exited
+
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(_handle_peer_connected)
+	multiplayer.peer_disconnected.connect(_handle_peer_disconnected)
+
+	GameNetwork.game_network_ready.emit()
+	load_world()
+
+
 func start() -> Result:
-	return Result.Err("missing implementation for `start`")
+	Logger.server_log(["starting server on: ", port], ["init"])
+	return Result.from_gderr(peer.create_server(port))
+
+
+#region Network
+func _handle_peer_connected(peer_id: int) -> void:
+	Logger.server_log(["client connected: ", peer_id], ["network"])
+	var spawn_result := Program.world.spawn_player({
+		"player_id": peer_id,
+	})
+	print(spawn_result)
+	if spawn_result.is_err():
+		Logger.server_log(["failed to spawn player(", peer_id, "): ", spawn_result.unwrap_err()], ["game"])
+
+
+func _handle_peer_disconnected(peer_id: int) -> void:
+	Logger.server_log(["client disconnected: ", peer_id], ["network"])
+	var unspawn_result := Program.world.unspawn_player(peer_id)
+	if unspawn_result.is_err():
+		Logger.server_log(["failed to unspawn player(", peer_id, "): ", unspawn_result.unwrap_err()], ["game"])
+#endregion
+
+
+@export var world_spawner: MultiplayerSpawner
+
+
+const DEFAULT_WORLD_SCENE := "res://world/game_world.tscn"
+func load_world(world_scene := DEFAULT_WORLD_SCENE) -> Result:
+	var game_world = load(world_scene).instantiate()
+	if not game_world is GameWorld:
+		return Result.Err("Node(" + world_scene + ") is not a `GameWorld` instance")
+	world_spawner.add_child(game_world)
+	return Result.Ok(null)
