66ad6fd1f03d1d3b8b39b7199d06e8a4073ff35a
Bryan Lee <bryanmylee@gmail.com>
2024-01-28 18:39:48 +0800

    feat: show user id on main screen

--- project/main/main.gd
+++ project/main/main.gd
@@ -1,8 +1,14 @@
 extends Node
 class_name Main
 
+@onready var user_name_label := $UI/Container/UserName as Label
+@onready var user_id_label := $UI/Container/UserId as Label
+
 
 func _ready() -> void:
 	var auth_result := await Authentication.initialize_default()
 	if auth_result.is_err():
 		print(auth_result.unwrap_err())
+	else:
+		user_name_label.text = Authentication.main_provider.user_name
+		user_id_label.text = Authentication.main_provider.user_id
