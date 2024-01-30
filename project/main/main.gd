b40a87ab6f27bd3e1cb41c64fed94cdce0843cf9
Bryan Lee <bryanmylee@gmail.com>
2024-01-30 15:38:59 +0800

    fix: load debug auth screen even when unauthenticated

--- project/main/main.gd
+++ project/main/main.gd
@@ -14,8 +14,7 @@ func _ready() -> void:
 	var auth_result := await Authentication.initialize_default()
 	if auth_result.is_err():
 		print(auth_result.unwrap_err())
-	else:
-		await load_debug_auth_screen()
+	await load_debug_auth_screen()
 
 
 func load_debug_auth_screen() -> void:
