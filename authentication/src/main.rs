1dfc04c390bc937de31f1f820d7fa83286f19b90
Bryan Lee <bryanmylee@gmail.com>
2024-02-07 04:28:15 +0800

    feat: configure db url via env vars

--- authentication/src/main.rs
+++ authentication/src/main.rs
@@ -38,9 +38,37 @@ async fn main() -> std::io::Result<()> {
 ///
 /// See more: <https://docs.rs/diesel/latest/diesel/r2d2/index.html>.
 fn initialize_db_pool() -> DbPool {
-    let conn_spec = std::env::var("DATABASE_URL").expect("DATABASE_URL should be set");
-    let manager = r2d2::ConnectionManager::<PgConnection>::new(conn_spec);
+    let db_url = get_db_url();
+    let manager = r2d2::ConnectionManager::<PgConnection>::new(db_url);
     r2d2::Pool::builder()
         .build(manager)
         .expect("DATABASE_URL should be a valid Postgres connection string")
+}
+
+fn get_db_url() -> String {
+    let url = std::env::var("POSTGRES_URL");
+    if let Ok(url) = url {
+        return url;
+    }
+
+    let password_text = std::env::var("POSTGRES_PASSWORD");
+    let password_file = std::env::var("POSTGRES_PASSWORD_FILE");
+    let password = if let Ok(password_text) = password_text {
+        password_text
+    } else {
+        let Ok(password_file) = password_file else {
+            panic!("Expected either POSTGRES_PASSWORD or POSTGRES_PASSWORD_FILE to be set");
+        };
+
+        use std::fs;
+        fs::read_to_string(password_file)
+            .expect("The file at POSTGRES_PASSWORD_FILE should contain the database password")
+    };
+
+    let user = std::env::var("POSTGRES_USER").unwrap_or("postgres".to_string());
+    let db = std::env::var("POSTGRES_DB").unwrap_or("postgres".to_string());
+    let host = std::env::var("POSTGRES_HOST").unwrap_or("localhost".to_string());
+    let port = std::env::var("POSTGRES_PORT").unwrap_or("5432".to_string());
+
+    format!("postgres://{user}:{password}@{host}:{port}/{db}")
 }
