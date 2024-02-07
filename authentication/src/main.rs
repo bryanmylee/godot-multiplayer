16d2b53e2d6da6b7326bfa356a69018932e7dc00
Bryan Lee <bryanmylee@gmail.com>
2024-02-08 01:08:30 +0800

    feat(auth): configure cors

--- authentication/src/main.rs
+++ authentication/src/main.rs
@@ -1,3 +1,4 @@
+use actix_cors::Cors;
 use actix_web::{get, middleware, web, App, HttpServer, Responder};
 use authentication::{auth, user, DbPool};
 use chrono::Duration;
@@ -25,10 +26,11 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(web::Data::new(pool.clone()))
             .app_data(web::Data::new(jwt_config.clone()))
-            .wrap(middleware::Logger::default())
             .wrap(middleware::NormalizePath::new(
                 middleware::TrailingSlash::Always,
             ))
+            .wrap(get_cors_config())
+            .wrap(middleware::Logger::default())
             .service(hello)
             .service(web::scope("/auth").configure(auth::config_service))
             .service(web::scope("/user").configure(user::config_service))
@@ -50,13 +52,13 @@ fn initialize_db_pool() -> DbPool {
 }
 
 fn get_db_url() -> String {
-    let url = std::env::var("POSTGRES_URL");
+    let url = env::var("POSTGRES_URL");
     if let Ok(url) = url {
         return url;
     }
 
-    let password_text = std::env::var("POSTGRES_PASSWORD");
-    let password_file = std::env::var("POSTGRES_PASSWORD_FILE");
+    let password_text = env::var("POSTGRES_PASSWORD");
+    let password_file = env::var("POSTGRES_PASSWORD_FILE");
     let password = if let Ok(password_text) = password_text {
         password_text
     } else {
@@ -68,17 +70,17 @@ fn get_db_url() -> String {
             .expect("The file at POSTGRES_PASSWORD_FILE should contain the database password")
     };
 
-    let user = std::env::var("POSTGRES_USER").unwrap_or("postgres".to_string());
-    let db = std::env::var("POSTGRES_DB").unwrap_or("postgres".to_string());
-    let host = std::env::var("POSTGRES_HOST").unwrap_or("localhost".to_string());
-    let port = std::env::var("POSTGRES_PORT").unwrap_or("5432".to_string());
+    let user = env::var("POSTGRES_USER").unwrap_or("postgres".to_string());
+    let db = env::var("POSTGRES_DB").unwrap_or("postgres".to_string());
+    let host = env::var("POSTGRES_HOST").unwrap_or("localhost".to_string());
+    let port = env::var("POSTGRES_PORT").unwrap_or("5432".to_string());
 
     format!("postgres://{user}:{password}@{host}:{port}/{db}")
 }
 
 fn get_jwt_config() -> auth::JwtConfig {
-    let secret_text = std::env::var("JWT_SECRET");
-    let secret_file = std::env::var("JWT_SECRET_FILE");
+    let secret_text = env::var("JWT_SECRET");
+    let secret_file = env::var("JWT_SECRET_FILE");
 
     let secret = if let Ok(secret_text) = secret_text {
         secret_text
@@ -91,11 +93,21 @@ fn get_jwt_config() -> auth::JwtConfig {
             .expect("The file at JWT_SECRET_FILE should contain the JWT signing secret")
     };
 
-    let expires_in_seconds = std::env::var("JWT_EXPIRES_IN")
+    let expires_in_seconds = env::var("JWT_EXPIRES_IN")
         .ok()
         .and_then(|p| p.parse::<i64>().ok())
         .unwrap_or(3600);
     let expires_in = Duration::seconds(expires_in_seconds);
 
     auth::JwtConfig { secret, expires_in }
+}
+
+fn get_cors_config() -> Cors {
+    let cors = Cors::permissive().supports_credentials();
+
+    if let Ok(origins) = env::var("ALLOWED_ORIGINS") {
+        origins.split(",").fold(cors, |cors, origin| cors.allowed_origin(origin))
+    } else {
+        cors
+    }
 }
