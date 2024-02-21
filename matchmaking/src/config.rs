99a502bb2c2ddd160c1de2460d419ed22289a5f5
Bryan Lee <bryanmylee@gmail.com>
2024-02-21 14:22:26 +0800

    chore(matchmaking): cors and identity config

--- matchmaking/src/config.rs
+++ matchmaking/src/config.rs
@@ -1,3 +1,6 @@
+use crate::identity::IdentityConfig;
+use actix_cors::Cors;
+use chrono::Duration;
 use std::env;
 
 #[allow(dead_code)]
@@ -29,7 +32,41 @@ fn get_required_secret_text_or_file(var: &str) -> String {
     }
 }
 
+fn get_identity_config() -> IdentityConfig {
+    let secret = get_required_secret_text_or_file("IDENTITY_SECRET");
+    let expires_in_seconds = get_secret_text_or_file("IDENTITY_EXPIRES_IN_SECS")
+        .and_then(|p| p.parse::<i64>().ok())
+        .unwrap_or(3600);
+    let expires_in = Duration::seconds(expires_in_seconds);
+
+    let refresh_secret = get_required_secret_text_or_file("REFRESH_SECRET");
+    let refresh_expires_in_days = get_secret_text_or_file("REFRESH_EXPIRES_IN_DAYS")
+        .and_then(|p| p.parse::<i64>().ok())
+        .unwrap_or(7);
+    let refresh_expires_in = Duration::days(refresh_expires_in_days);
+
+    IdentityConfig {
+        secret,
+        expires_in,
+        refresh_secret,
+        refresh_expires_in,
+    }
+}
+
 lazy_static::lazy_static! {
     pub static ref POSTGRES_URL: String = get_required_secret_text_or_file("POSTGRES_URL");
     pub static ref GAME_SERVER_MANAGER_SERVICE_KEY: String = get_required_secret_text_or_file("GAME_SERVER_MANAGER_SERVICE_KEY");
+    pub static ref IDENTITY_CONFIG: IdentityConfig = get_identity_config();
+}
+
+pub fn get_cors_config() -> Cors {
+    let cors = Cors::permissive().supports_credentials().allow_any_method();
+
+    if let Some(origins) = get_secret_text_or_file("ALLOWED_ORIGINS") {
+        origins
+            .split(",")
+            .fold(cors, |cors, origin| cors.allowed_origin(origin))
+    } else {
+        cors
+    }
 }
