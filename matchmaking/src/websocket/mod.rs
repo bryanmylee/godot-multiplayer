98d032360b9de0706aed3b4232fee1af49cc6e71
Bryan Lee <bryanmylee@gmail.com>
2024-02-21 23:16:35 +0800

    chore(matchmaking): move ws route to `/listen`

--- matchmaking/src/websocket/mod.rs
+++ matchmaking/src/websocket/mod.rs
@@ -1,2 +1,32 @@
 pub mod server;
 pub mod session;
+
+use self::{server::WebsocketServer, session::WebsocketSession};
+use crate::identity::{Identity, IdentityConfig};
+use actix::Addr;
+use actix_web::{error, get, web, HttpRequest, Responder};
+use actix_web_actors::ws;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct ListenParams {
+    token: String,
+}
+
+#[get("/listen/")]
+async fn listen(
+    params: web::Query<ListenParams>,
+    req: HttpRequest,
+    stream: web::Payload,
+    server_address: web::Data<Addr<WebsocketServer>>,
+    id_config: web::Data<IdentityConfig>,
+) -> actix_web::Result<impl Responder> {
+    let identity = Identity::from_token(&id_config, &params.token)?;
+
+    ws::start(
+        WebsocketSession::new(identity.user_id, server_address.as_ref().clone()),
+        &req,
+        stream,
+    )
+    .map_err(error::ErrorInternalServerError)
+}
