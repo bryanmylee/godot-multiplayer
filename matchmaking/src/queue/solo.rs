fe618027a5a35885be2174a683d8183dddc1bf79
Bryan Lee <bryanmylee@gmail.com>
2024-02-22 00:58:51 +0800

    test(matchmaking): test harness setup for solo queue

--- matchmaking/src/queue/solo.rs
+++ matchmaking/src/queue/solo.rs
@@ -41,3 +41,54 @@ async fn leave(
 
     Ok(HttpResponse::Ok().json(player))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        identity::{Identity, IdentityService},
+        queue,
+    };
+    use actix_web::{http::header::AUTHORIZATION, test, App};
+    use std::sync::Arc;
+    use uuid::Uuid;
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn server_sends_ready_message_to_clients_when_ready() {
+        struct RandomIdService;
+
+        impl IdentityService for RandomIdService {
+            fn get_identity(&self, _: &BearerToken) -> Result<Identity, actix_web::error::Error> {
+                Ok(Identity {
+                    user_id: Uuid::new_v4(),
+                })
+            }
+        }
+
+        let id_service = web::Data::from(Arc::new(RandomIdService) as Arc<dyn IdentityService>);
+        let queue_data = web::Data::new(queue::QueueData::new());
+
+        let app = test::init_service(
+            App::new()
+                .app_data(id_service)
+                .app_data(queue_data)
+                .service(join),
+        )
+        .await;
+
+        let req = test::TestRequest::post()
+            .insert_header((AUTHORIZATION, "Bearer 0000"))
+            .uri("/join/")
+            .to_request();
+        test::call_service(&app, req).await;
+
+        let req = test::TestRequest::post()
+            .insert_header((AUTHORIZATION, "Bearer 0000"))
+            .uri("/join/")
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+
+        println!("{:?}", test::read_body(resp).await);
+    }
+}
